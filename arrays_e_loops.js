513033fdcb1182edf482e2d9e8137e9056648b1a
Lostleleco <alex_soares_oliveira@hotmail.com>
2025-02-18 20:06:39 -0300

    Pratices

---
+++ arrays_e_loops.js
@@ -0,0 +1,26 @@
+// incrementando de 1 em 1
+for (var teste = 1; teste <=10; teste++){
+    console.log(teste)
+}
+
+
+var VideoGames = ['swithc', 'ps4', 'xbox', 'nintendo', 'playstation', 'nintendo'];
+
+for (var game = 0; VideoGames.length <=5; game++){
+    console.log(VideoGames[game])
+} 
+
+var frutas = ['banana', 'maca', 'pera' ];
+{
+    frutas.forEach(function(fruta, index) {
+        console.log(fruta, index)
+    })
+}
+
+//Exercicio
+
+var copa = ['Alemanha', 'Brasil', 'Egito', 'Uruguai', 'Portugal', 'Italia'];
+
+for (var i = 0; i < copa.length; i++) {
+    console.log(copa[i]);
+}
